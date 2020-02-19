478cda18c0b81a93ee9d9c52eca9240843be8a17
spencerpomme <spencerpomme@live.com>
2020-02-19 19:00:32 +0800

    rename topic.prefix for debug purpose

--- producers/connector.py
+++ producers/connector.py
@@ -45,7 +45,7 @@ def configure_connector():
                "table.whitelist": "stations",
                "mode": "incrementing",
                "incrementing.column.name": "stop_id",
-               "topic.prefix": "connect->",
+               "topic.prefix": "connector->",
                "poll.interval.ms": "10000",
            }
        }),
