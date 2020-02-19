2705b10f1e09b667fe28a7a572a3f143f019a856
spencerpomme <spencerpomme@live.com>
2020-02-19 15:44:42 +0800

    Step4-1: start
    remove the comments

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -33,16 +33,16 @@ class TransformedStation(faust.Record):
 #   places it into a new topic with only the necessary information.
 app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
 # TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
+topic = app.topic("TODO", value_type=Station)
 # TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
+out_topic = app.topic("TODO", partitions=1)
 # TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+table = app.Table(
+   # "TODO",
+   # default=TODO,
+   partitions=1,
+   changelog_topic=out_topic,
+)
 
 
 #
