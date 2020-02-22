84d23ac646c61bea339c1b6e1cc0bd7d4ac805dc
spencerpomme <spencerpomme@live.com>
2020-02-22 11:21:00 +0800

    update simulation

--- producers/simulation.py
+++ producers/simulation.py
@@ -14,8 +14,8 @@ import pandas as pd
 logging.config.fileConfig(f"{Path(__file__).parents[0]}/logging.ini")
 
 from connector import configure_connector
-from models.line import Line
-from models.weather import Weather
+from models import Line
+from models import Weather
 
 
 logger = logging.getLogger(__name__)
